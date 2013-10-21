1653a9597377e08f01fe95b93f4b04457a3cc705
tlerias <tlerias@gmail.com>
2013-10-21 16:20:53 -0400

    added more comments

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,14 +8,14 @@ n = gets
 # Processing
 if n != nil
   1.upto(n.to_i) do |i|
-	  if i % 3 == 0 && i % 5 == 0
+	  if i % 3 == 0 && i % 5 == 0 #fizzbuzz if divisible by 3 & 5
 		  puts 'fizzbuzz'
-	  elsif i % 5 == 0
+	  elsif i % 5 == 0 # buzz if divisible by 5
 		  puts 'buzz'
-	  elsif i % 3 == 0
+	  elsif i % 3 == 0 #fizz if divisible by 3
 		  puts 'fizz'
 	  else
-		  puts i
+		  puts i #Output
 	  end
   end
 end
